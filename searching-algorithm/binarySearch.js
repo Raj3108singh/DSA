16a0085ed7e69c74849f4d16592aad0558f3c41c
rs <singh.rajan3108@gmail.com>
2024-08-24 11:27:08 +0530

    added steps for binary search

--- searching-algorithm/binarySearch.js
+++ searching-algorithm/binarySearch.js
@@ -1,3 +1,12 @@
+/**
+ * In Binary search , we divide the array in two parts from the middle and then call the search function
+ * again using recursion with these two arrays and repeat the process till we get the result.
+ *
+ * to Calculate the middle part we use (start + (end-start)/2).
+ */
+
+// here start is the starting index of array, end is the ending index and key is the value we are looking for.
+
 function binarySearch(arr, start, end, key) {
   let middleIndex = Math.floor(start + (end - start) / 2);
   if (start > end) return 'Not Found';
