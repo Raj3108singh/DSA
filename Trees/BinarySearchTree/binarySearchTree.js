b50158f1d5f28a364281c03aaa8fada0315913b3
rs <singh.rajan3108@gmail.com>
2024-08-29 19:56:40 +0530

    bst insert

---
+++ Trees/BinarySearchTree/binarySearchTree.js
@@ -0,0 +1,56 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value === current.value) return undefined; // to handle duplicate insertion
+        if (value < currentNode.value) {
+          if (currentNode.left === null) {
+            currentNode.left = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.left;
+          }
+        } else if (value > currentNode.value) {
+          if (currentNode.right === null) {
+            currentNode.right = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.right;
+          }
+        } else if (value === currentNode.value) {
+          return currentNode;
+        }
+      }
+    }
+  }
+}
+
+//      10
+//   5     13
+// 2  7  11  16
+
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
